f4e5335e517edf56c9eeff125b87a68d303df789
SidyMohamedSalim <sidymamadousalim@gmail.com>
2023-06-24 16:37:38 +0100

    add dynamic home

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,16 @@
 import Image from "next/image";
+import { prisma } from "../src/db/prisma";
 
-export default function Home() {
+export default async function Home() {
+  const articles = await prisma.article.findMany({
+    take: 6,
+  });
+  const newArticles = await prisma.article.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+    take: 3,
+  });
   return (
     <>
       <section className="bg-head-bg bg-cover  ">
@@ -105,166 +115,105 @@ export default function Home() {
         </div>
 
         <div className="py-6 grid grid-cols-4 gap-2">
-          {/* grid 1 */}
-          <div className="col-span-4 row-span-1 rounded-md shadow-2xl flex justify-between ">
-            <div className="w-[200%]">
-              <img
-                src="/img1.jpg"
-                alt=""
-                className="w-full h-full rounded-md"
-              />
-            </div>
-            <div className="px-10 py-2 text-start">
-              <h1 className="font-bold py-3 text-xl">
-                Traditional French Cuisine
-              </h1>
-              <p className="text-sm font-light py-2 font-sans">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Adipisci corrupti ea maiores eveniet nesciunt! Ex nobis
-                consequuntur deleniti fugiat sequi nam cupiditate, dolor
-              </p>
-              <div className="flex mt-5">
-                <img
-                  src="/avatar.jpg"
-                  alt=""
-                  className="w-10 h-10 rounded-full"
-                />
-                <div className="mx-2">
-                  <h1 className="font-bold text-sm">Salim Diallo</h1>
-                  <p className="text-xs font-extralight italic">12 Mai 2000</p>
+          {articles.map((article, index) => {
+            if (index === 0) {
+              /* grid 1 */
+              return (
+                <div className="col-span-4 row-span-1 rounded-md shadow-2xl flex justify-between ">
+                  <div className="w-[200%]">
+                    <img
+                      src={article.image ?? `/images${article.title}`}
+                      alt={article.title}
+                      className="w-full h-full rounded-md"
+                    />
+                  </div>
+                  <div className="px-10 py-2 text-start">
+                    <h1 className="font-bold py-3 text-xl">{article.title}</h1>
+                    <p className="text-sm font-light py-2 font-sans">
+                      {article.description}
+                    </p>
+                    <div className="flex mt-5">
+                      <img
+                        src="/avatar.jpg"
+                        alt=""
+                        className="w-10 h-10 rounded-full"
+                      />
+                      <div className="mx-2">
+                        <h1 className="font-bold text-sm">Salim Diallo</h1>
+                        <p className="text-xs font-extralight italic">
+                          12 Mai 2000
+                        </p>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-          </div>
-          {/* grid 2 */}
-          <div className=" rounded-md shadow-2xl max-md:col-span-2">
-            <div className="w-[100%]">
-              <img src="/img2.jpg" alt="" className="rounded-md w-[100%]" />
-            </div>
-            <div className="px-2 py-2 text-start">
-              <h1 className="font-bold py-3 text-sm">
-                Traditional French Cuisine
-              </h1>
-              <p className="text-xs font-light py-1 font-sans">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              </p>
-              <div className="flex mt-1">
-                <img
-                  src="/avatar.jpg"
-                  alt=""
-                  className="w-8 h-8 rounded-full"
-                />
-                <div className="mx-2">
-                  <h1 className="font-bold text-xs">Salim Diallo</h1>
-                  <p className="text-xs font-extralight italic">12 Mai 2000</p>
+              );
+            } else if (index === 2) {
+              /* grid 3 */
+              return (
+                <div className="col-span-2 row-span-2 rounded-md shadow-2xl">
+                  <div className="w-full">
+                    <img
+                      src={article.image ?? `/images${article.title}`}
+                      alt={article.title}
+                      className="rounded-md"
+                    />
+                  </div>
+                  <div className="px-10 py-2 text-start">
+                    <h1 className="font-bold py-3 text-xl">{article.title}</h1>
+                    <p className="text-sm font-light py-2 font-sans">
+                      {article.description}
+                    </p>
+                    <div className="flex mt-5">
+                      <img
+                        src="/avatar.jpg"
+                        alt=""
+                        className="w-10 h-10 rounded-full"
+                      />
+                      <div className="mx-2">
+                        <h1 className="font-bold text-sm">Salim Diallo</h1>
+                        <p className="text-xs font-extralight italic">
+                          12 Mai 2000
+                        </p>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-          </div>
-          {/* grid 3 */}
-          <div className="col-span-2 row-span-2 rounded-md shadow-2xl">
-            <div className="w-full">
-              <img src="/img3.jpg" alt="" className="rounded-md" />
-            </div>
-            <div className="px-10 py-2 text-start">
-              <h1 className="font-bold py-3 text-xl">
-                Traditional French Cuisine
-              </h1>
-              <p className="text-sm font-light py-2 font-sans">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Adipisci corrupti ea maiores eveniet nesciunt! Ex nobis
-                consequuntur deleniti fugiat sequi nam cupiditate, dolor
-              </p>
-              <div className="flex mt-5">
-                <img
-                  src="/avatar.jpg"
-                  alt=""
-                  className="w-10 h-10 rounded-full"
-                />
-                <div className="mx-2">
-                  <h1 className="font-bold text-sm">Salim Diallo</h1>
-                  <p className="text-xs font-extralight italic">12 Mai 2000</p>
+              );
+            } else {
+              return (
+                /* grid 2 */
+                <div className=" rounded-md shadow-2xl max-md:col-span-2">
+                  <div className="w-[100%]">
+                    <img
+                      src={article.image ?? `/images${article.title}`}
+                      alt={article.title}
+                      className="rounded-md w-[100%]"
+                    />
+                  </div>
+                  <div className="px-2 py-2 text-start">
+                    <h1 className="font-bold py-3 text-sm">{article.title}</h1>
+                    <p className="text-xs font-light py-1 font-sans">
+                      {article.description}
+                    </p>
+                    <div className="flex mt-1">
+                      <img
+                        src="/avatar.jpg"
+                        alt=""
+                        className="w-8 h-8 rounded-full"
+                      />
+                      <div className="mx-2">
+                        <h1 className="font-bold text-xs">Salim Diallo</h1>
+                        <p className="text-xs font-extralight italic">
+                          12 Mai 2000
+                        </p>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-          </div>
-          {/* grid 4 */}
-          <div className=" rounded-md shadow-2xl max-md:col-span-2">
-            <div className="w-[100%]">
-              <img src="/img4.jpg" alt="" className="rounded-md w-[100%]" />
-            </div>
-            <div className="px-2 py-2 text-start">
-              <h1 className="font-bold py-3 text-sm">
-                Traditional French Cuisine
-              </h1>
-              <p className="text-xs font-light py-1 font-sans">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              </p>
-              <div className="flex mt-1">
-                <img
-                  src="/avatar.jpg"
-                  alt=""
-                  className="w-8 h-8 rounded-full"
-                />
-                <div className="mx-2">
-                  <h1 className="font-bold text-xs">Salim Diallo</h1>
-                  <p className="text-xs font-extralight italic">12 Mai 2000</p>
-                </div>
-              </div>
-            </div>
-          </div>
-          {/* grid 5 */}
-          <div className=" rounded-md shadow-2xl max-md:col-span-2">
-            <div className="w-[100%]">
-              <img src="/img5.jpg" alt="" className="rounded-md w-[100%]" />
-            </div>
-            <div className="px-2 py-2 text-start">
-              <h1 className="font-bold py-3 text-sm">
-                Traditional French Cuisine
-              </h1>
-              <p className="text-xs font-light py-1 font-sans">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              </p>
-              <div className="flex mt-1">
-                <img
-                  src="/avatar.jpg"
-                  alt=""
-                  className="w-8 h-8 rounded-full"
-                />
-                <div className="mx-2">
-                  <h1 className="font-bold text-xs">Salim Diallo</h1>
-                  <p className="text-xs font-extralight italic">12 Mai 2000</p>
-                </div>
-              </div>
-            </div>
-          </div>
-          {/* grid 6 */}
-          <div className=" rounded-md shadow-2xl max-md:col-span-2">
-            <div className="w-[100%]">
-              <img src="/img6.jpg" alt="" className="rounded-md w-[100%]" />
-            </div>
-            <div className="px-2 py-2 text-start">
-              <h1 className="font-bold py-3 text-sm">
-                Traditional French Cuisine
-              </h1>
-              <p className="text-xs font-light py-1 font-sans">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              </p>
-              <div className="flex mt-1">
-                <img
-                  src="/avatar.jpg"
-                  alt=""
-                  className="w-8 h-8 rounded-full"
-                />
-                <div className="mx-2">
-                  <h1 className="font-bold text-xs">Salim Diallo</h1>
-                  <p className="text-xs font-extralight italic font-sans">
-                    12 Mai 2000
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
+              );
+            }
+          })}
         </div>
       </section>
 
@@ -277,47 +226,23 @@ export default function Home() {
         </div>
         <div className="flex justify-between max-sm:flex-wrap">
           {/* news bloc */}
-          <div className="mx-4 shadow-2xl max-sm:my-5">
-            <div>
-              <img src="/img1.jpg" alt="" className="rounded-md" />
+          {newArticles.map((article, index) => (
+            <div className="mx-4 shadow-2xl max-sm:my-5 sm:w-[35%]">
+              <div>
+                <img
+                  src={article.image ?? `/images/${article.title}`}
+                  alt={article.title}
+                  className="rounded-md sm:w-[100]"
+                />
+              </div>
+              <div className="px-2 py-1 text-start">
+                <h1 className="font-bold text-xl py-4">{article.title}</h1>
+                <p className="font-sans font-light text-xs">
+                  {article.description}
+                </p>
+              </div>
             </div>
-            <div className="px-2 py-1 text-start">
-              <h1 className="font-bold text-xl py-4">Elemy no graisse add</h1>
-              <p className="font-sans font-light text-xs">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
-                sint qui placeat harum in id commodi quas laborum quidem unde
-                blanditiis voluptatibus ea, explicabo,
-              </p>
-            </div>
-          </div>
-          {/* news bloc */}
-          <div className="mx-4 shadow-2xl max-sm:my-5">
-            <div>
-              <img src="/img1.jpg" alt="" className="rounded-md" />
-            </div>
-            <div className="px-2 py-1 text-start">
-              <h1 className="font-bold text-xl py-4">Elemy no graisse add</h1>
-              <p className="font-sans font-light text-xs">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
-                sint qui placeat harum in id commodi quas laborum quidem unde
-                blanditiis voluptatibus ea, explicabo,
-              </p>
-            </div>
-          </div>
-          {/* news bloc */}
-          <div className="mx-4 shadow-2xl max-sm:my-5">
-            <div>
-              <img src="/img1.jpg" alt="" className="rounded-md" />
-            </div>
-            <div className="px-2 py-1 text-start">
-              <h1 className="font-bold text-xl py-4">Elemy no graisse add</h1>
-              <p className="font-sans font-light text-xs">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
-                sint qui placeat harum in id commodi quas laborum quidem unde
-                blanditiis voluptatibus ea, explicabo,
-              </p>
-            </div>
-          </div>
+          ))}
         </div>
       </section>
 
