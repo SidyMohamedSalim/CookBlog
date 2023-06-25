f135a9e61beddf3180352eabaefd440ac3235b5b
SidyMohamedSalim <sidymamadousalim@gmail.com>
2023-06-25 11:07:58 +0100

    resolve problem deploy4

--- app/admin/add/page.tsx
+++ app/admin/add/page.tsx
@@ -4,7 +4,7 @@ import FormAdd from "./formAdd";
 const page = async () => {
   return (
     <div className="max-w-4xl max-xl:px-3 mx-auto text-center pt-32 pb-7 font-sans">
-      <h1 className="font-bold text-2xl">Ajout d'un Article</h1>
+      {/* <h1 className="font-bold text-2xl">Ajout d'un Article</h1> */}
       <p className="italic  py-4 ">
         <span className="border-b border-black">Directive:</span> A la fin de
         chaque paragraphe mettre un slash
--- app/admin/edit/[id]/page.tsx
+++ app/admin/edit/[id]/page.tsx
@@ -13,7 +13,7 @@ const page = async ({ params }: { params: { id: string } }) => {
   }
   return (
     <div className="max-w-4xl max-xl:px-3 mx-auto text-center pt-32 pb-7 font-sans">
-      <h1 className="font-bold text-2xl">Modifier l'Article</h1>
+      {/* <h1 className="font-bold text-2xl">Modifier l'Article</h1> */}
       <FormModify article={article} />
     </div>
   );
