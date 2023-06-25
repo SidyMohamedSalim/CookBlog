88718bfd0f9b7e12496742d0645f3cf2bf857018
SidyMohamedSalim <sidymamadousalim@gmail.com>
2023-06-25 10:07:51 +0100

    change design article

--- app/articles/[id]/page.tsx
+++ app/articles/[id]/page.tsx
@@ -23,17 +23,18 @@ const page = async ({ params }: { params: { id: string } }) => {
   return (
     <div>
       <section className="max-w-4xl max-xl:px-3 mx-auto text-center pt-32 pb-7 ">
+        <h1 className="text-4xl font-bold py-6">type {article.title}</h1>
         <img
           src={article.image ? article.image : `/images/${article.title}.png`}
           alt=""
           className="mx-auto rounded-md"
         />
-        <h1 className="text-2xl font-bold py-6">type {article.title}</h1>
-
-        <div className="text-start font-sans font-light text-sm">
-          <p className="py-3">{article.description}</p>
+        <div className="text-start font-sans font-light text-sm py-6">
+          <p className="py-3 text-center italic bg-slate-100 px-3 rounded-r-xl">
+            "{article.description}"
+          </p>
           {paragraphs.map((para) => (
-            <p className="py-3 indent-6 text-lg">{para}</p>
+            <p className="py-3 my-4 indent-6 text-lg">{para}</p>
           ))}
         </div>
         {author && (
