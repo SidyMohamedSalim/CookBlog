56d9483d1db968fe03b0290c7c9585f9ee783ce3
SidyMohamedSalim <sidymamadousalim@gmail.com>
2023-06-25 11:05:51 +0100

    resolve problem deploy3

--- app/admin/add/page.tsx
+++ app/admin/add/page.tsx
@@ -4,11 +4,11 @@ import FormAdd from "./formAdd";
 const page = async () => {
   return (
     <div className="max-w-4xl max-xl:px-3 mx-auto text-center pt-32 pb-7 font-sans">
-      <h1 className="font-bold text-2xl"> Ajout d'un Article</h1>
+      <h1 className="font-bold text-2xl">Ajout d'un Article</h1>
       <p className="italic  py-4 ">
-        <span className="border-b border-black"> Directive:</span> A la fin de
+        <span className="border-b border-black">Directive:</span> A la fin de
         chaque paragraphe mettre un slash
-        {/* <span className="text-orange-600 font-extrabold"> </span> */}
+        <span className="text-orange-600 font-extrabold"> &ldquo;</span>
       </p>
       <FormAdd />
     </div>
